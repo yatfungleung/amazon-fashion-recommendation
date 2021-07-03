fc8048b6993986025dc5c66b0d3ea4cb313f691e
Yat Fung (Abraham) Leung <yatfung.leung@outlook.com>
2021-07-03 12:32:38 +0800

    update notebook

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ import pandas as pd
 import pickle
 from PIL import Image
 
-image0 = Image.open('image/streamlit/calvin_klein_poster.jpg')
-st.image(image0, use_column_width='always')
+img = Image.open('image/streamlit/calvin_klein_poster.jpg')
+st.image(img, use_column_width='always')
 
 st.title('Amazon Fashion Recommender')
 
-st.write('created by Abraham Leung')
+st.write('created by Abraham Leung')  
+st.write('-------------------------')
 
-image1 = Image.open('image/streamlit/abraham_leung_logo.jpg')
-st.sidebar.image(image1, width=300)
+img = Image.open('image/streamlit/abraham_leung_logo.jpg')
+st.sidebar.image(img, width=300)
 
 st.sidebar.title('Business Value:')
 st.sidebar.write('''
@@ -33,8 +34,8 @@ st.sidebar.title('Contact:')
 linkedin1, linkedin2 = st.sidebar.beta_columns([1,4])
 
 with linkedin1:
-    image2 = Image.open('image/streamlit/linkedin_logo.png')
-    st.image(image2, width=30)
+    img = Image.open('image/streamlit/linkedin_logo.png')
+    st.image(img, width=30)
 
 with linkedin2:
     link1 = "[Abraham's LinkedIn](https://www.linkedin.com/in/abraham-leung-data-science)"
@@ -43,10 +44,41 @@ with linkedin2:
 github1, github2 = st.sidebar.beta_columns([1,4])
 
 with github1:
-    image3 = Image.open('image/streamlit/github_logo.png')
-    st.image(image3, width=30)
+    img = Image.open('image/streamlit/github_logo.png')
+    st.image(img, width=30)
 
 with github2:
     link2 = "[Abraham's GitHub](https://github.com/yatfungleung)"
     st.markdown(link2, unsafe_allow_html=True)
 
+# testing for activewear
+
+# load data
+df_activewear = pd.read_csv('data/activewear_recommender.csv')
+
+for i in range(len(df_activewear)):
+
+    col0, col1 = st.beta_columns((1,2))
+
+    with col0:
+        img = Image.open(df_activewear['img_file'][i])
+        st.image(img)
+
+    with col1:
+        st.write(df_activewear['name'][i])
+
+    col0, col1, col2, col3 = st.beta_columns((1,2,1,2))
+
+    with col0:
+        img = Image.open(df_activewear['recommend_img_file1'][i])
+        st.image(img)
+
+    with col1:
+        st.write(df_activewear['recommend_name1'][i])
+    
+    with col2:
+        img = Image.open(df_activewear['recommend_img_file2'][i])
+        st.image(img)
+
+    with col3:
+        st.write(df_activewear['recommend_name2'][i])
